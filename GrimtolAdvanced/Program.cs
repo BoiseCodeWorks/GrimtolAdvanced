b8199b4f94ea7c915428f1b40a48469709753659
Jake Overall <joverall22@gmail.com>
2018-10-02 21:34:20 -0600

    Add project files.

---
+++ GrimtolAdvanced/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GrimtolAdvanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
